e9142acab2d1e334e71229adfc0bc91685a76a89
Kenan Yildirim <kenan@kenany.me>
2014-10-19 14:35:32 -0400

    Use battery-path
    Closes #3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var split = require('binary-split');
 var fs = require('graceful-fs');
 var isFunction = require('lodash.isfunction');
 var firstLine = require('first-line');
+var batteryPath = require('battery-path');
 
 function present(battery, callback) {
   if (isFunction(battery)) {
@@ -10,7 +11,7 @@ function present(battery, callback) {
     battery = 'BAT0';
   }
 
-  var bstr = path.resolve('/sys/class/power_supply/', battery, 'present');
+  var bstr = path.resolve(batteryPath(battery), 'present');
 
   firstLine(bstr, function(error, line) {
     if (error) {
